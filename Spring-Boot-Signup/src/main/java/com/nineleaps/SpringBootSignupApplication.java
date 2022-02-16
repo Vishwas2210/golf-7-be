628ae92bc5024588fdb78e85b35908e7a522f3db
Vishwas2210 <vishwas.agarwal@nineleaps.com>
2022-02-16 10:43:19 +0530

    First Commit

---
+++ Spring-Boot-Signup/src/main/java/com/nineleaps/SpringBootSignupApplication.java
@@ -0,0 +1,13 @@
+package com.nineleaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSignupApplication.class, args);
+	}
+
+}
