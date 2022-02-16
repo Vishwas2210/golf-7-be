66cc596eef62fc31b9f91b8caed356639eac502e
Vishwas2210 <vishwas.agarwal@nineleaps.com>
2022-02-16 11:35:39 +0530

    Name updated of the project

--- House-Rental-app/src/test/java/com/nineleaps/HouseRentalAppApplicationTests.java
+++ House-Rental-app/src/test/java/com/nineleaps/HouseRentalAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootSignupApplicationTests {
+class HouseRentalAppApplicationTests {
 
 	@Test
 	void contextLoads() {
