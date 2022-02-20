33d59d1db159795da6a6b08b0a448ec797fd284f
JayantKC <jayant.khanchandani@nineleaps.com>
2022-02-20 20:29:40 +0530

    Added Validation

--- House-Rental-app/src/main/java/com/nineleaps/model/User.java
+++ House-Rental-app/src/main/java/com/nineleaps/model/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
@@ -18,15 +19,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotEmpty
     @Column(name = "first_name", nullable = false)
     private String firstName;
+
     @Column(name = "last_name", nullable = true)
     private String lastName;
+
+    @NotEmpty
     @Column(name = "email", nullable = false)
     @Email
     private String email;
+
+    @NotEmpty
     @Column(name = "phone", nullable = false)
     private String phone;
+
+    @NotEmpty
     @Column(name = "password", nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Size(min = 6, message = "Password Should have at least 6 characters.")
